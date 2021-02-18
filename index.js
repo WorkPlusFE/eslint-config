6c524fd73a774434eb7b57a3e3268dd2f51187f5
lwy1010 <im.leewy@gmail.com>
2021-02-18 15:18:27 +0800

    feat: add custom config rules

--- index.js
+++ index.js
@@ -13,12 +13,30 @@ module.exports = {
     browser: true,
   },
 
+  parser: 'vue-eslint-parser',
+
   parserOptions: {
     ecmaVersion: 12,
     parser: '@typescript-eslint/parser',
     sourceType: 'module',
   },
 
-  // 在这里定义你的规则风格，定义规则时请为其提供注释说明
-  rules: {},
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-alert': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-restricted-syntax': 'off',
+    'import/no-cycle': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'import/prefer-default-export': 'off',
+    'class-methods-use-this': 'off',
+    'object-curly-newline': 'off',
+    'no-param-reassign': 'off',
+    'implicit-arrow-linebreak': 'off',
+    'consistent-return': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+  },
 };
